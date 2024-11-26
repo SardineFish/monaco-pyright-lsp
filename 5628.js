45717d4137959d2694392f2f9ba28b5015f6c0c4
SardineFish <sardine@live.cn>
2024-11-26 14:30:32 +0800

    Create example pages
    Signed-off-by: SardineFish <sardine@live.cn>

---
+++ 5628.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_webpack_project=self.webpackChunkmy_webpack_project||[]).push([[5628],{45628:(e,c,p)=>{p.r(c)}}]);
---
+++ typing.d.ts
@@ -0,0 +1,9 @@
+declare module '*.zip' {
+    const contents: ArrayBuffer;
+    export = contents;
+}
+declare module '*.egg' {
+    const contents: ArrayBuffer;
+    export = contents;
+}
+//# sourceMappingURL=typing.d.ts.map
